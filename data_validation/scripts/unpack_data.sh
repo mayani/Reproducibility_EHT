21207d61bbf07fd8733b1d1df4b0d1de90bfd31d
Rajiv Mayani <mayani@isi.edu>
2024-04-04 15:33:46 -0700

    Fetch inputs from EHT repo

--- data_validation/scripts/unpack_data.sh
+++ data_validation/scripts/unpack_data.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 mkdir -p images
 mkdir -p data
+wget https://github.com/eventhorizontelescope/2019-D01-01/raw/master/EHTC_FirstM87Results_Apr2019_uvfits.tgz
+wget https://github.com/eventhorizontelescope/2019-D01-01/raw/master/EHTC_FirstM87Results_Apr2019_csv.tgz
+tar -xvzf EHTC_FirstM87Results_Apr2019_uvfits.tgz -C data --strip-components=1
 tar -xvzf EHTC_FirstM87Results_Apr2019_csv.tgz -C data --strip-components=1
 mkdir -p data/csv/converted
